353e9507c005a81f9f3816dc6e6b3a6821c759e7
pkdandan <haorongyi@126.com>
2016-07-07 17:17:24 +0800

    Add files via upload

---
+++ src/main/java/com/zj/reward/pojo/BannerVO.java
@@ -0,0 +1,46 @@
+package com.zj.reward.pojo;
+
+import java.util.Date;
+
+public class BannerVO {
+
+	private int id;
+	private String name;
+	private String description;
+	private String type;
+	private Date createDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
+	
+	
+}
